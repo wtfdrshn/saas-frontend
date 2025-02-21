4f7df345410f6f08d0aeeea9c68245c579d5b391
Darshan <itsmedarshan8@gmail.com>
2025-02-22 00:59:50 +0530

    CORS fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://evenhub.azurewebsites.net/api',
   withCredentials: true,
-  
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
 });
 
 api.interceptors.request.use(
