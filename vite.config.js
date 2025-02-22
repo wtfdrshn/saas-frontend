e8cf24c4795917027952e58c306dcea6d6707a01
Darshan <itsmedarshan8@gmail.com>
2025-02-22 10:50:07 +0530

    fixes (CORS)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': "http://localhost:5000"
+      '/api': import.meta.env.VITE_API_URL
     }
   },
   define: {
